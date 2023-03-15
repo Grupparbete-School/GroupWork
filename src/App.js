d9e567a33b49aad2e50167f04bf371ea03622326
malin eriksson <erixon.malin@hotmail.com>
2023-03-15 10:24:33 +0100

    Navbar fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import AppRouter from './components/AppRouter';
+// import Login from './test/test';
 
 export default function App() {
 
   return (
     <section className="App">
         <AppRouter />
+      {/* <div>
+            <Login />
+          </div> */}
     </section>
   );
 };
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -10,19 +10,19 @@ import LogOut from '../pages/LogOut';
 import {HashRouter, Route, Routes} from "react-router-dom";
 
 export default function AppRouter (){
-    return(
-        <HashRouter>
-        <Navbar>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/report" element={<Report />} />
-            <Route path="/information" element={<Information />} />
-            <Route path="/myprofil" element={<MyProfil />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/logout" element={<LogOut />} />
-          </Routes>
-        </Navbar>
-      </HashRouter> 
-    );
+  return(
+    <HashRouter>
+      <Navbar>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/information" element={<Information />} />
+          <Route path="/myprofil" element={<MyProfil />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/logout" element={<LogOut />} />
+        </Routes>
+      </Navbar>
+    </HashRouter> 
+  );
 };
