e4b1b8779ed21bbeafc553fb695f906a9f570bc7
ellvon <pellinor99@hotmail.com>
2023-03-16 09:21:59 +0100

    Login frontend changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AppRouter from './components/AppRouter';
+import Login from './components/Login';
 // import Login from './test/test';
 
 export default function App() {
 
   return (
     <section className="App">
-        <AppRouter />
+        {/* <AppRouter /> */}
+        <Login/>
       {/* <div>
             <Login />
           </div> */}
