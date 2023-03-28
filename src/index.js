3c8144a50b071631194b24769a5dabcd8042220e
ewahlstrom90@gmail.com <ewahlstrom90@gmail.com>
2023-03-28 22:01:19 +0200

    Fixed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import FetchPeople from './test/testFunktion';
 import ActiveProjects from './test/ActiveProjects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-     
-    <div>
-        <App />
-    <div>
-    {/* <ActiveProjects /> */}
-    </div>
-    </div>
+     <ActiveProjects />
+    // <div>
+    //     <App />
+    // <div>
+    // {/* <ActiveProjects /> */}
+    // </div>
+    // </div>
 );
