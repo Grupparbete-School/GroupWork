0c0a8c9e2eac14680f4dc53af64d70bcd544d577
malin eriksson <erixon.malin@hotmail.com>
2023-03-30 21:09:08 +0200

    Final fixes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import FetchPeople from './test/testFunktion';
-import ActiveProjects from './test/ActiveProjects';
-
+// import App from './App';
+// import FirstPage from './components/FirstPage'
+import TimeAdd from './pages/TimeAdd'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-     
-    <div>
-        <App />
-    <div>
-    {/* <ActiveProjects /> */}
-    </div>
-    </div>
+    <TimeAdd />
 );
