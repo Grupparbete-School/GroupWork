4f43d621f6e6b41599dae3cf7b4fcdd659eb4b2b
ullzten <ullzten@gmail.com>
2023-04-01 01:27:59 +0200

    Fixat i AddComment.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ActiveProjects from './test/ActiveProjects';
+import AddComment from './test/AddComment';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-     <ActiveProjects />
+    <AddComment/>
+    //  <ActiveProjects />
     // <div>
     //     <App />
     // <div>
