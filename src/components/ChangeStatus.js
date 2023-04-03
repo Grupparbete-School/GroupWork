e85c469496f78d24a532409b2232c8a578869b63
Aldor B <vlad.ur@live.se>
2023-04-03 16:24:28 +0200

    errors fixed

--- src/components/ChangeStatus.js
+++ src/components/ChangeStatus.js
@@ -110,7 +110,7 @@ export default function ChangeStatus() {
           Klicka på "Välj ett projekt" för att se mer info och ändra statusen.</h6>
         </div>
       
-      <div className="flex items-center h-screen text-black">
+      <div className="flex items-center h-screen text-black" style={{height: "auto", padding: "50px 10px"}}>
         <div className="mx-auto text-center card-container2">
           <h1 className="text-center">Ändra status<br/><br/></h1>
           
