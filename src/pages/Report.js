73ebe328a7f326c73edf9bbfd30d2344a4ff3d25
Madde <105611442+SpiritualMaddie@users.noreply.github.com>
2023-04-03 13:46:16 +0200

    fixes

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Navbar from '../components/Navbar'; import Navbar2 from '../components/Navbar2';
+import Navbar from '../components/Navbar'; 
+import Navbar2 from '../components/Navbar2';
 import ChangeTime from "../components/ChangeTime";
 import TimeAdd from '../components/TimeAdd';
 
