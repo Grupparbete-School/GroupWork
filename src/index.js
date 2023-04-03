a9976741ab96cb9ece4a58cf8990ab2a0875c9ec
ullzten <ullzten@gmail.com>
2023-04-03 22:06:23 +0200

    added map for projectImages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import ProjectImage from './components/ProjectImagesMap'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />  
+   // <App />  
+<ProjectImage/>
 );
